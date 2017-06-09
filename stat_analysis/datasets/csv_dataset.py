4f3c712c38d5e6e14e7a0b4fe954af5f52e7e83c
TomJamesGray <tomg77@yahoo.co.uk>
2017-06-09 23:27:01 +0100

    Added Column names

--- stat_analysis/datasets/csv_dataset.py
+++ stat_analysis/datasets/csv_dataset.py
@@ -2,8 +2,10 @@ import csv
 from stat_analysis.datasets.dataset import DataSet
 
 class CSVDataset(DataSet):
-    def __init__(self,fname):
+    def __init__(self,fname,col_names):
         self.fname = fname
+        self.col_names = col_names
+        super(CSVDataset,self).__init__(col_names)
 
     def __iter__(self):
         with open(self.fname,'r') as f:
--- stat_analysis/main.py
+++ stat_analysis/main.py
@@ -1,6 +1,9 @@
 import argparse
+import logging
 from stat_analysis.datasets import csv_dataset
-def main(args):
+
+logger = logging.getLogger(__name__)
+def main(cl_args):
     """
     Main entry point for the program
     :param args: String of command line arguments, excluding program name
@@ -8,12 +11,11 @@ def main(args):
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("file")
-    args = parser.parse_args(args)
-    print(args.file)
+    parser.add_argument("col_names")
+    args = parser.parse_args(cl_args)
+    logger.info("Data set file name: {}".format(args.file))
 
-    d = csv_dataset.CSVDataset(args.file)
+    d = csv_dataset.CSVDataset(args.file,args.col_names)
 
-    for x in d:
-        print(x)
 
     return True
