71f2cb0a4c82fd4f62767edd7de95f1e8f1529a4
TomJamesGray <tomg77@yahoo.co.uk>
2017-08-03 21:37:17 +0100

    Moved project to focus on web interface using flask for the server

--- stat_analysis/main.py
+++ stat_analysis/main.py
@@ -1,21 +1,12 @@
 import argparse
 import logging
 from stat_analysis.datasets import csv_dataset
+from flask import Flask
+from flask import render_template
 
 logger = logging.getLogger(__name__)
-def main(cl_args):
-    """
-    Main entry point for the program
-    :param args: String of command line arguments, excluding program name
-    :return:
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file")
-    parser.add_argument("col_names")
-    args = parser.parse_args(cl_args)
-    logger.info("Data set file name: {}".format(args.file))
+app = Flask(__name__)
 
-    d = csv_dataset.CSVDataset(args.file,args.col_names)
-
-
-    return True
+@app.route("/")
+def index():
+    return render_template("index.html")
