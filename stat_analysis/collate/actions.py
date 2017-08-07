315d995d2a9a6ebdcb1b24e51d9bd2f286fed901
TomJamesGray <tomg77@yahoo.co.uk>
2017-08-07 11:46:54 +0100

    Added try except block for sum actions

--- stat_analysis/collate/actions.py
+++ stat_analysis/collate/actions.py
@@ -2,9 +2,13 @@ def sum(data,col_name):
     output = {}
     for key,value in data.items():
         for data_item in value:
-            if key in output:
-                output[key] += int(data_item[col_name])
-            else:
-                output[key] = int(data_item[col_name])
+            try:
+                if key in output:
+                    output[key] += int(data_item[col_name])
+                else:
+                    output[key] = int(data_item[col_name])
+            except Exception as e:
+                print("Exception : {} raised at {}".format(e,data_item))
+
 
     return output
